1040a0f27b0956851ddf93b6d85cac5d5bdeca51
LABCOM_6 <suraidah218@gmail.com>
2017-11-17 11:21:59 +0700

    testing upload to Github

---
+++ src/myfirstJAVA.java
@@ -0,0 +1,14 @@
+package oop_lab.s359211110128.rmutvs.com;
+
+public class myfirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word, I'm DA");
+        System.out.println("Information System");
+        System.out.println("Faculty of Managrment Technology");
+        System.out.println("RMUTSV");
+    }// main
+
+
+
+}// class
